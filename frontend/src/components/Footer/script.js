3597e56973d90d8aa2320cab0eac72b4291c9b28
fabiosvd <fabiosalvador85@yahoo.com.br>
2022-02-15 13:38:03 -0300

    created footer component

---
+++ frontend/src/components/Footer/script.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './styles.scss';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div class="footer-content">
+        <p></p>
+        <a href="https://github.com/fabiosvd" target="_blank">
+          © Fabio Salvador 2022
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
