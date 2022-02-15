ac96d55ce9a096d4a8d0db44744febbda4e003bb
fabiosvd <fabiosalvador85@yahoo.com.br>
2022-02-15 12:37:32 -0300

    added product data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,3 +1,56 @@
-const products = [];
+const products = [
+  {
+    name: 'Popular Hoodie',
+    imageUrl:
+      'https://i.pinimg.com/originals/1b/36/b7/1b36b74eec13cba252aac5db1b9c2a2f.jpg',
+    description:
+      'A closet staple, the Popular Hoodie combines a classic embroidered logo on the left chest with the soft comfort of fleece for an elevated everyday look.',
+    price: 120.0,
+    category: 'Hoodies',
+  },
+  {
+    name: 'Blank Hoodie',
+    imageUrl:
+      'https://i.pinimg.com/originals/68/d2/b0/68d2b07556fc64ebf0603f481a3f8b7f.jpg',
+    description:
+      'A closet staple, the Blank Hoodie combines a classic embroidered logo on the left chest with the soft comfort of fleece for an elevated everyday look.',
+    price: 120.0,
+    category: 'Hoodies',
+  },
+  {
+    name: 'Camo Hoodie',
+    imageUrl:
+      '//cdn.shopify.com/s/files/1/0337/9413/0052/products/Mens-Camo-Hoodie-4_2048x2048.jpg?v=1638840259',
+    description:
+      'A closet staple, the Camo Hoodie combines a classic embroidered logo on the left chest with the soft comfort of fleece for an elevated everyday look.',
+    price: 120.0,
+    category: 'Hoodies',
+  },
+  {
+    name: 'Goals Hoodie',
+    imageUrl:
+      '//cdn.shopify.com/s/files/1/0337/9413/0052/products/plainhoodie_stepup_setgoals_goforit_2048x2048.jpg?v=1607694496',
+    description:
+      'A closet staple, the Goals Hoodie combines a classic embroidered logo on the left chest with the soft comfort of fleece for an elevated everyday look.',
+    price: 120.0,
+    category: 'Hoodies',
+  },
+  {
+    name: 'Dab Hoodie',
+    imageUrl: 'https://m.media-amazon.com/images/I/71ABETA6brL._UX679_.jpg',
+    description:
+      'A closet staple, the Dab Hoodie combines a classic embroidered logo on the left chest with the soft comfort of fleece for an elevated everyday look.',
+    price: 120.0,
+    category: 'Hoodies',
+  },
+  {
+    name: 'Blue Hoodie',
+    imageUrl: 'https://m.media-amazon.com/images/I/31Dw1PDRu8S.jpg',
+    description:
+      'A closet staple, the Blue Hoodie combines a classic embroidered logo on the left chest with the soft comfort of fleece for an elevated everyday look.',
+    price: 120.0,
+    category: 'Hoodies',
+  },
+];
 
 module.exports = products;
