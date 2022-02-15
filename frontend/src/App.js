5ffca83c3091b1a916cb729279b57368fee1e47b
fabiosvd <fabiosalvador85@yahoo.com.br>
2022-02-15 13:43:35 -0300

    added products route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/products" component={ProductsPage} />
       </Switch>
     </Router>
   );
