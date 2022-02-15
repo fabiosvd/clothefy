d029491b2f1f5be7331d5a7b01dc1c5d8d295c1f
fabiosvd <fabiosalvador85@yahoo.com.br>
2022-02-15 12:37:05 -0300

    created product schema

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,27 @@
 const mongoose = require('mongoose');
 
-const productSchema = mongoose.Schema({});
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+});
 
 const Product = mongoose.model('product', productSchema);
 
