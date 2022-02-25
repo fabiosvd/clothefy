5acde1bffc15b5bbda4ead1ae042fbb43b544205
fabiosvd <fabiosalvador85@yahoo.com.br>
2022-02-25 14:03:12 -0300

    added heroku

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const productRoutes = require('./routes/productRoutes');
 const connectDB = require('./config/db');
+const path = require('path');
 
 connectDB();
 
@@ -9,11 +10,19 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: 'API running...' });
-});
-
 app.use('/api/products', productRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.json({ message: 'API running...' });
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
